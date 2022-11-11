92094d48c27f17929ffe6d364b649b0d1fcb8302
Abdulraheem Ahmad <abdulahmad586@gmail.com>
2022-11-12 00:15:51 +0100

    New commit

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app
 
 app
     .get('/', (req, res) => {
-        res.status(200).json({ message: 'Welcome to our Node server ---> Ready for production, Speak spanish? try /hola' });
+        res.status(200).json({ message: 'Welcome to our Node server ---> Ready for production, Speak spanish? try /hola. Orchestrator image is now available on dockerhub' });
     })
     .get('/hola', (req, res) => {
         res.status(200).json({ message: 'Bienvenido a nuestro servidor de nodos ---> Listo para la producción' });
